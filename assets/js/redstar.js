18f688c79bfc0fd83d2c010ec279526f7641dd96
Darshil vastarpara <vastarparadarshil@gmail.com>
2024-07-05 15:21:59 +0530

    v1.1.1

---
+++ assets/js/redstar.js
@@ -0,0 +1,31 @@
+
+const fname = document.getElementById('fname');
+const contact = document.getElementById('contact');
+const email = document.getElementById('email');
+// add event listner on form container...
+const submit = document.getElementsByClassName('form-data')[0];
+
+console.log(fname);
+
+submit.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    console.log("clicked");
+    // write html to proper format email sent   
+    let ebody = `
+    <b>Name: </b>${fname.value}
+    <br><b>Contact: </b>${contact.value}
+    <br><b>Email: </b>${email.value}
+
+    `
+
+    // email code here 
+    Email.send({
+        SecureToken : "b0896956-c927-4561-b0da-600185189394",
+        To : 'vastarparadarshil@gmail.com',
+        From : "vastarparadarshil@gmail.com",
+        Subject : "testing mail" + email.value,
+        Body : ebody
+    }).then(
+      message => alert(message)
+    );
+});
