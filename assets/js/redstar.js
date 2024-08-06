3bb9cf2cbad630a2b3c254cbd368aec3659deae7
DarshilVastarpara <vastarparadarshil@gmail.com>
2024-08-06 15:51:56 +0530

    v2

--- assets/js/redstar.js
+++ assets/js/redstar.js
@@ -4,30 +4,43 @@ const contact = document.getElementById('contact');
 const email = document.getElementById('email');
 const msg = document.getElementById('msg');
 
+
 // add event listner on form container...
 const submit = document.getElementsByClassName('form-data')[0];
 
-console.log(fname);
 
-submit.addEventListener('submit',(e)=>{
-    e.preventDefault();
-    console.log("clicked");
-    // write html to proper format email sent   
-    let ebody = `
+
+submit.addEventListener('submit', (e) => {
+
+  e.preventDefault();
+  console.log("clicked");
+  document.getElementById('fname').clear;
+
+
+
+  let ebody = `
     <b>Name: </b>${fname.value}
     <br><b>Contact: </b>${contact.value}
     <br><b>Email: </b>${email.value}
     <br><b>Message: </b>${msg.value}
     `
 
-    // email code here 
-    Email.send({
-        SecureToken : "b0896956-c927-4561-b0da-600185189394",
-        To : 'vastarparadarshil@gmail.com',
-        From : "vastarparadarshil@gmail.com",
-        Subject : "testing mail" + email.value,
-        Body : ebody
-    }).then(
-      message => alert(message)
-    );
+  // email code here 
+  Email.send({
+      SecureToken : "b0896956-c927-4561-b0da-600185189394",
+      To : 'vastarparadarshil@gmail.com',
+      From : "vastarparadarshil@gmail.com",
+      Subject : "testing mail" + email.value,
+      Body : ebody
+  }).then(
+    message => alert(message)
+
+  );
 });
+document.getElementById("myForm").addEventListener("submit", function (event) {
+  document.getElementById("myForm").reset();
+  event.preventDefault();
+});
+
+
+// clear the code inpute fiels 
